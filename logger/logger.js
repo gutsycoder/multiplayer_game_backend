c55b657a6883e4059572fe5507abd6f3365dfde7
gutsycoder <suyashsincever13@gmail.com>
2023-08-20 18:35:46 +0530

    Corrected The Logger Format

--- logger/logger.js
+++ logger/logger.js
@@ -2,10 +2,16 @@ const winston = require('winston');
 
 const logger = winston.createLogger({
     level: 'error',
+    format: winston.format.combine(
+        winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+        winston.format.errors({ stack: true }),
+        winston.format.printf(({ timestamp, level, message, stack }) => {
+            return `${timestamp} [${level}] - ${message}\n${stack || ''}`;
+        })
+    ),
     transports: [
         new winston.transports.File({
             filename: 'errors.log',
-            timestamp: true,
         }),
     ],
 });
--- middleware/checkAuth.js
+++ middleware/checkAuth.js
@@ -3,24 +3,21 @@ const jwt = require('jsonwebtoken');
 const logger = require('../logger/logger')
 
 const authMiddleware = (req,res,next)=>{
-
-    const headerAuth = req.headers['Authorization'];
-
-    const parts = headerAuth.split(' ');
-
-    // Check if the token is present and in the correct format
-    if (parts.length !== 2 || parts[0] !== 'Bearer') {
-      return res.status(401).json({ message: 'Unauthorized', data: [] });
+    try{
+        const headerAuth = req.headers['Authorization'];
+        const parts = headerAuth.split(' ');
+        // Check if the token is present and in the correct format
+        if (parts.length !== 2 || parts[0] !== 'Bearer') {
+        return res.status(401).json({ message: 'Unauthorized', data: [] });
 
     }
     const token = parts[1];
 
     if(!token){
         return res.status(401).json({message:"Unauthorized",data:[]});
-    }
-
-    try{
-        const decoded = jwt.verify(token,process.env.JWT_SECRET);
+    }   
+    
+    const decoded = jwt.verify(token,process.env.JWT_SECRET);
 
         if(!decoded){
             return res.status(401).json({message:"Unauthorized",data:[]});
@@ -30,7 +27,7 @@ const authMiddleware = (req,res,next)=>{
         req.userData.name=decoded.name;
         next();
 
-    }catch(error){
+ }catch(error){
         logger.error(error);
         console.error(error);
         return res.status(500).json({message:"Unauthorized",data:[]});
